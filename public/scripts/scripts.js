f53c5bc44286a657bcf5a107989a0090175e6c04
James Logue <logueja@gmail.com>
2018-03-12 14:51:45 -0600

    Add html structure

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,13 @@
-console.log('yoooo')
+const generatePalette = () => {
+  console.log('generate')
+}
+
+const addPalette = event => {
+  event.preventDefault()
+  console.log('addPalette')
+}
+
+const addProject = event => {
+  event.preventDefault()
+  console.log('addProject')
+}
