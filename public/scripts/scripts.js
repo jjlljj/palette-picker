1ac6499445cd9d81b7e4907e7847c5c913f487b3
James Logue <logueja@gmail.com>
2018-03-12 18:38:39 -0600

    Update color lock to persist when new palette is generated, able to lock multiple colors

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -22,39 +22,47 @@ const updatePalette = () => {
   })
 
   newPalette.map(color => renderColor(color.color))
+  toSto(newPalette)
 }
 
 const renderColor = color => {
   const colorsWrap = document.querySelector(".colors-wrap")
   const colorDiv = document.createElement("div")
   colorDiv.setAttribute("class", "color-card")
-  colorDiv.setAttribute("style", `background-color: ${color}`)
+  colorDiv.setAttribute("style", `background-color: #${color}`)
+
+  console.log(color)
   colorDiv.innerHTML = `
     <button
-      onclick="lockColor()"
+      type="button" 
+      onClick="lockColor(event)"
+      name="${color}"
       class="color-lock-btn"
     >LOCK</button>
-    <h3>${color}</h3>
+    <h3>#${color}</h3>
   `
   colorsWrap.appendChild(colorDiv)
 }
 
-const lockColor = id => {
+const lockColor = event => {
+  event.preventDefault()
+  const id = event.target.name
   const colorPalette = fromSto()
-  console.log(colorPalette)
-  //const newPalette = colorPalette.map(color => {
-  // if (color.color === id) {
-  //  return {...color, lock: !color.lock}
-  //} else {
-  //  return color
-  //}
-  //})
 
-  //toSto(newPalette)
+  
+  const newPalette = colorPalette.map(color => {
+    if (color.color === id) {
+      return {...color, lock: !color.lock}
+    } else {
+      return color
+    }
+  })
+
+  toSto(newPalette)
 }
 
 const getRandomColor = () => {
-  return '#'+Math.random().toString(16).slice(-6) 
+  return Math.random().toString(16).slice(-6) 
 }
 
 const clearPalette = () => {
