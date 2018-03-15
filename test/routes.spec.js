d3c32cf4173aa340dab6338d9b81787558b3b439
James Logue <logueja@gmail.com>
2018-03-15 15:45:53 -0600

    Begin testing setup

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,4 +7,34 @@ chai.use(chaiHttp)
 
 const environment = process.env.NODE_ENV || 'test'
 const configuration = require('../knexfile')[environment]
-const database = require('knex')(configuration)
+const db = require('knex')(configuration)
+
+describe('Client Routes', () => {
+  before( done => {
+    db.migrate.rollback()
+      .then( () => {
+        db.migrate.latest()
+      })
+      .then( () => {
+        done()
+      })
+  })
+
+  beforeEach( done => {
+    db.seed.run()
+  })
+
+  it('should return the homepage', () => {
+
+    return chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200)
+        response.should.be.html
+      })
+      .catch( error => {
+        throw error
+      })
+  })
+
+})
