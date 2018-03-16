adcf74162c02aed41a992e50093cfa1f8855700b
James Logue <logueja@gmail.com>
2018-03-16 09:53:55 -0600

    Add initial comments

--- server.js
+++ server.js
@@ -1,60 +1,59 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express')  // importing the express to use the express server 
+const app = express()  // declaring a new intance of the express server 
+const bodyParser = require('body-parser')  // importing body-parser middle ware so app has access to post body
 
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const db = require('knex')(configuration)
+const environment = process.env.NODE_ENV || 'development'  // declaring the node environment to work in, in this case dev
+const configuration = require('./knexfile')[environment]  // pull in knex config file to configure dev environment
+const db = require('knex')(configuration)  // curry instance of db to knex so db can be called with knex methods
 
-app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Palette Picker'
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.set('port', process.env.PORT || 3000)  // set the port the server will run on, either set in the node environment, or default 3000
+app.locals.title = 'Palette Picker'  // name the app 'palette picker', used in error responses and server logs
+app.use(bodyParser.json())  // apply bodyparser as middleware for all endpoints
+app.use(express.static('public'))  // specify path for serving of front end js and html client assets
 
-app.get('/', (request, response) => {
+app.get('/', (request, response) => {  // empty get route, used for serve of client assets, as above
 })
 
-app.get('/api/v1/projects', (request, response) => {
-  const { projects } = app.locals
+app.get('/api/v1/projects', (request, response) => {  // endpoint for fetching all projects, expects .get verb at the specified url 
   
-  db('projects').select()
-    .then(projects => {
-      response.status(200).json(projects)
+  db('projects').select() // select the projects db using the knex method .select
+    .then(projects => {  // await the db, then arrow function to do something with db response
+      response.status(200).json(projects) // send db response as json as response from endpoint with status 200
     })
-    .catch(error => {
-      response.status(500).json({ error })
+    .catch(error => { // await any errors
+      response.status(500).json({ error }) // handle any errors, send error as json with status 500
     })
 })
 
-app.post('/api/v1/projects', (request, response) => {
-  const { project } = request.body
-  if (!project['name']) {
-    return response.status(422).send({ error: "Expected format: { name: <String> }. You're missing a name property"})
+app.post('/api/v1/projects', (request, response) => { // endpoint for adding a project, expects .post verb at the specified url 
+  const { project } = request.body  // destructure project key from request body
+  if (!project['name']) {  // check for required property name from project sent in request body
+    return response.status(422).send({ error: "Expected format: { name: <String> }. You're missing a name property"}) // send 522 response, specifying missing property if property is not present in request body 
   }
 
-  db('projects').insert(project, 'id')
-    .then(dbProject => {
-      response.status(201).json({ id: dbProject[0], name: project.name })
+  db('projects').insert(project, 'id') // query database projects, insert the project recieved in post request to db, expect insert to return the id of the inserted project
+    .then(dbProject => {  // await db action, arrow function to do something with db response
+      response.status(201).json({ id: dbProject[0], name: project.name }) // send db response as json with project name as response from endpoint with status 201
     })
-    .catch(error => {
-      response.status(500).json({ error })
-    })
-})
-
-
-app.get('/api/v1/palettes', (request, response) => {
-  db('palettes').select()
-    .then(palettes => {
-      response.status(200).json(palettes)
-
-    })
-    .catch(error => {
-      response.status(500).json({ error }) 
+    .catch(error => {  // await any errors
+      response.status(500).json({ error })  // handle any errors, send error as json with status 500
     })
 })
 
-app.get('/api/v1/palettes/:id', (request, response) => {
-  const { id } = request.params
+
+app.get('/api/v1/palettes', (request, response) => { // endpoint for fetching all palettes, expects .get verb at the specified url 
+  db('palettes').select()  // select the palettes db using the knex method .select
+    .then(palettes => {  // await the db, then arrow function to do something with db response
+      response.status(200).json(palettes)  // send db response as json as response from endpoint with status 200
+
+    })
+    .catch(error => {  // await any errors
+      response.status(500).json({ error })  // handle any errors, send error as json with status 500 
+    })
+})
+
+app.get('/api/v1/palettes/:id', (request, response) => { // endpoint for fetch a palette based on the palette id, expects .get verb at the specified url, where id is dynamic within the endpoint 
+  const { id } = request.params  // destructure id from request params (url)
 
   db('palettes').where("id", id).select()
     .then(palette => {
