ba70beb891d8b2e759e9baa8ca43a842a2218797
James Logue <logueja@gmail.com>
2018-03-20 10:27:57 -0600

    update middleware

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const db = require('knex')(configuration)
-  
+
 const httpsRedirect = (request, response, next) => {
-  if (request.headers.protocol !== "https://"){
-    response.redirect("https://" + request.headers.host + request.path);
+  if(request.protocol !== "https://"  && environment === 'production') {
+    response.redirect("https://" + request.headers.host + "/" + request.path);
   }
   next()
 }
